62875c8880f311471e59126814670b1551b16a90
EKataykina <ekataikina@ya.ru>
2024-04-17 17:53:05 +0100

    Описание изменений

---
+++ configuration.py
@@ -0,0 +1,4 @@
+URL_SERVICE = "https://8354b5df-4fdb-413f-870d-5b276c3eb666.serverhub.praktikum-services.ru"
+CREATE_COURIER = "/api/v1/courier"
+CREATE_ORDER = "/api/v1/orders"
+ORDER_INFORMATION = "/api/v1/orders/track?t="
---
+++ data.py
@@ -0,0 +1,13 @@
+order_body = {
+    "firstName": "Денис",
+    "lastName": "Борсук",
+    "address": "Алтуфьевское шоссе 89-301",
+    "metroStation": 152,
+    "phone": "+7 952 529 27 75",
+    "rentTime": 2,
+    "deliveryDate": "2024-04-18",
+    "comment": "Домофон по номеру квартиры",
+    "color": [
+        "BLACK"
+    ]
+}
